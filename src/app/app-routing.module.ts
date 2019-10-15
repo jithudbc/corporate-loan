75b3dae8dc300205224fb9233e6407c77b8ccc3d
sateesh9292 <murapaka.sateesh@gmail.com>
2019-10-15 11:03:07 +0000

    added services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './core/component/login/login.component';
 import { LoanComponent } from './core/component/loan/loan.component';
+import { DashboardComponent } from './core/component/dashboard/dashboard.component';
 
 
 const routes: Routes = [
   { path:'',component:LoanComponent},
-  { path:'login',component:LoginComponent}
+  { path:'login',component:LoginComponent},
+  { path: 'dashboard', component:DashboardComponent}
 ];
 
 @NgModule({
